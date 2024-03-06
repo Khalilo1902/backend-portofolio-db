99ccbbbf40a096dbe092852a31ac9d11729d6b3b
Khalil <Khalil.haouas@gmail.com>
2024-03-06 23:25:43 +0100

    finish

--- dist/index.js
+++ dist/index.js
@@ -12,21 +12,10 @@ const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config();
 (0, dbConnect_1.default)();
 const app = (0, express_1.default)();
-app.use((0, cors_1.default)({
-    credentials: true,
-    origin: function (origin, callback) {
-        // Hier können Sie überprüfen, ob der Origin-Header akzeptiert werden soll
-        if (origin === 'https://khalil-dev.me') {
-            callback(null, true);
-        }
-        else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    }
-}));
+app.use((0, cors_1.default)({ credentials: true, origin: "https://khalil-dev.me" }));
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
 app.use(userRouter_1.default);
 const PORT = process.env.PORT || 3003;
-app.listen(PORT, () => console.log(`Server is running on PORT http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on PORT https://khalil-dev.me:${PORT}`));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -12,21 +12,13 @@ dotenv.config()
 dbConnect()
 
 const app = express()
-app.use(cors({
-    credentials: true,
-    origin: function (origin, callback) {
-      // Hier können Sie überprüfen, ob der Origin-Header akzeptiert werden soll
-      if (origin === 'https://khalil-dev.me') {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    }
-  }));app.use(express.json())
+app.use(cors({credentials: true , origin:"https://khalil-dev.me"}))
+    
+app.use(express.json())
 app.use(cookieParser())
 
 app.use(userRouter)
 
 const PORT = process.env.PORT || 3003
 
-app.listen(PORT,()=>console.log(`Server is running on PORT http://localhost:${PORT}`))
+app.listen(PORT,()=>console.log(`Server is running on PORT https://khalil-dev.me:${PORT}`))
