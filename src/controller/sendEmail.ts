2f3fe26c310b42773c2e7e8cd7796d30674c16ae
Khalil <Khalil.haouas@gmail.com>
2024-03-07 12:16:25 +0100

    finish

--- src/controller/sendEmail.ts
+++ src/controller/sendEmail.ts
@@ -26,7 +26,7 @@ const sendEmail = async (email:string,firstName:string,verifyToken:string) => {
                     <h1 style="color: #333;">Click the link below to verify your account</h1>
                     <img src='https://th.bing.com/th/id/OIP.cneIxMB7ie6Q1JkJoLB5zwHaE2?w=279&h=183&c=7&r=0&o=5&dpr=1.3&pid=1.7' alt='brand' width='40' height='40' />
                     <p style="color: #666;">Please click the link below to verify your account:</p>
-                    <a href="https://khalil-dev.me/user/verify_account/${verifyToken}" style="display: inline-block; padding: 10px 20px; background-color: #007BFF; color: #fff; text-decoration: none; border-radius: 5px;">Verify Account</a>
+                    <a href="https://khalil-dev.me/verify_account/${verifyToken}" style="display: inline-block; padding: 10px 20px; background-color: #007BFF; color: #fff; text-decoration: none; border-radius: 5px;">Verify Account</a>
                 </div>`
 
     })
